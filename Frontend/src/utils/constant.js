38f7da74406b4128bd97e59eca9ed2ba51c0254e
Abhishek kumar <kumarabhishek28147@gmail.com>
2025-06-13 06:27:56 +0530

    url update

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,9 +1,9 @@
 
 
 
-export const USER_API_END_POINT="http://localhost:8080/api/users";
-export const JOB_API_END_POINT="http://localhost:8080/api/jobs";
-export const COMPANY_API_END_POINT="http://localhost:8080/api/company";
-export const APPLICATION_API_END_POINT="http://localhost:8080/api/application";
+export const USER_API_END_POINT="https://hiresphere-qpd5.onrender.com/api/users";
+export const JOB_API_END_POINT="https://hiresphere-qpd5.onrender.com/api/jobs";
+export const COMPANY_API_END_POINT="https://hiresphere-qpd5.onrender.com/api/company";
+export const APPLICATION_API_END_POINT="https://hiresphere-qpd5.onrender.com/api/application";
 
 
